ad519e86d9550f0b3b5eec0b4317c132ec5a05db
James <jamesmuerle@gmail.com>
2012-11-10 03:25:51 -0800

    Had to add files?

--- src/main/webapp/application.js
+++ src/main/webapp/application.js
@@ -17,17 +17,6 @@ require(['dojox/cometd', 'dojo/dom', 'dojo/domReady!'], function(cometd, dom)
     	}
     });
     
-    cometd.addListener('/broadcast/games', function(message){
-    	console.log(message);
-    });
-    
-    dom.byId('greeter').onclick = function()
-    {
-        cometd.publish('/service/hello', 'John Smith');
-        cometd.publish('/service/games', 'John Smith');
-    };
-
-    
     cometd.handshake();
 });
 
--- src/main/webapp/waiting/application.js
+++ src/main/webapp/waiting/application.js
@@ -9,30 +9,37 @@ require(['dojox/cometd', 'dojo/dom', 'dojo/domReady!'], function(cometd, dom)
     {
     	if (message.successful)
     	{
-    		dom.byId('status').innerHTML += '<div>CometD handshake successful</div>';
+    		console.log('<div>CometD handshake successful</div>');
+    	    cometd.publish('/broadcast/waiting', get_username());
+    		cometd.subscribe('/broadcast/waiting', update_users);
     	}
     	else
     	{
-    		dom.byId('status').innerHTML += '<div>CometD handshake failed</div>';
+    		console.log('<div>CometD handshake failed</div>');
     	}
     });
-    
-    dom.byId('hello_server').onclick = function() {
-        cometd.publish('/service/hello', 'Hello, World');
-    };
     
     cometd.handshake();
     fill_in_user();
 });
 
-function fill_in_user() {
+function get_username() {
 	var username = document.cookie.split(";");
 	for (i in username) {
 		kvpair = username[i].split("=");
 		kvpair[0] = kvpair[0].replace(/\s+/g, '');
 		if (kvpair[0] === "uName") {
-			document.getElementById("username").innerHTML = kvpair[1];
+			return kvpair[1];
 		}
+	}
+}
+
+function update_users(event) {
+	var pplList = event.data;
+	var ul = document.getElementById("user_list");
+	ul.innerHTML = "\n";
+	for (i in pplList) {
+		ul.innerHTML += "<li>" + pplList[i] + "</li>\n";
 	}
 }
 
